936b92ae56ecdd4e6980527a73dbd9a2ee745180
pspkzar <cesar.perdigao92@gmail.com>
2014-02-24 03:09:25 +0000

    Initial Project

---
+++ main.cpp
@@ -0,0 +1,504 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GL/freeglut.h>
+#include <IL/il.h>
+
+#include <assimp/Importer.hpp>
+#include <assimp/material.h>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+
+#include <optixu/optixpp_namespace.h>
+#include <optixu/optixu.h>
+#include <optixu/optixu_matrix_namespace.h>
+#include <optixu/optixu_math_namespace.h>
+#include <optixu/optixu_vector_types.h>
+
+#define ANISOTROPY 0.0f
+
+using namespace optix;
+
+int width=720;
+int height=720;
+
+Context renderer;
+Buffer out;
+
+Assimp::Importer importer;
+std::string scene_p="crytek-sponza/";
+std::string scene_name="sponza.obj";
+std::string ptx_p="rt.ptx";
+
+enum ray_types
+{
+    Shadow,
+    Phong,
+    RAY_TYPE_COUNT,
+};
+
+Buffer genOutputBuffer()
+{
+    RTformat format = RT_FORMAT_FLOAT4;
+    Buffer outBuffer=renderer->createBuffer(RT_BUFFER_OUTPUT,format,width,height);
+    outBuffer->validate();
+    return outBuffer;
+}
+
+void reshape(int w, int h)
+{
+    width=w;
+    height=h;
+    glViewport(0,0,w,h);
+    //Pass Arguments to Optix
+    out->setSize(w,h);
+
+}
+
+inline void optix_draw()
+{
+    renderer->launch(0,width,height);
+    void *pixels=out->map();
+    glDrawPixels(width,height,GL_RGBA,GL_FLOAT,pixels);
+    out->unmap();
+}
+
+void renderScene()
+{
+    //Render with Optix
+    //clear buffer
+    glClear(GL_COLOR_BUFFER_BIT);
+    //optix
+    optix_draw();
+    //fps count
+    static int frame=0, time,timebase=0;
+
+    frame++;
+    time=glutGet(GLUT_ELAPSED_TIME);
+    if(time-timebase){
+        float fps=frame*1000.0/(time-timebase);
+        timebase=time;
+        frame=0;
+
+        std::cout<<fps<<" FPS"<<std::endl;
+    }
+    //swap buffers
+    glutSwapBuffers();
+}
+
+inline const aiScene* loadScene(std::string scene_path)
+{
+    std::ifstream scene_file(scene_path.c_str());
+    if(!scene_file.fail())
+    {
+        scene_file.close();
+    }
+    else
+    {
+        std::cout<<"Error reading scene file."<<std::endl;
+        return NULL;
+    }
+
+    const aiScene *s=importer.ReadFile(scene_path,aiProcessPreset_TargetRealtime_MaxQuality);
+
+    if(!s)
+    {
+        std::cout<<"Failed to load scene: "<<importer.GetErrorString()<<std::endl;
+        return NULL;
+    }
+    std::cout<<"Scene loaded sccessfully."<<std::endl;
+
+    return s;
+}
+
+TextureSampler newTexture(std::string name)
+{
+    ILuint image=ilGenImage();
+    ilBindImage(image);
+    TextureSampler res;
+    ILboolean success=ilLoadImage((ILstring)(scene_p+name).c_str());
+    if(success){
+        res=renderer->createTextureSampler();
+        ilConvertImage(IL_RGBA,IL_FLOAT);
+        int w=ilGetInteger(IL_IMAGE_WIDTH);
+        int h=ilGetInteger(IL_IMAGE_HEIGHT);
+        void * data= (void*)ilGetData();
+        Buffer tex = renderer->createBuffer(RT_BUFFER_INPUT,RT_FORMAT_FLOAT4,w,h);
+        void * dataMap = tex->map();
+        ILint size = ilGetInteger(IL_IMAGE_SIZE_OF_DATA);
+        memcpy(dataMap,data,size);
+        ilBindImage(0);
+        ilDeleteImage(image);
+        tex->unmap();
+        tex->validate();
+        res->setWrapMode(0,RT_WRAP_REPEAT);
+        res->setWrapMode(1,RT_WRAP_REPEAT);
+        res->setReadMode(RT_TEXTURE_READ_ELEMENT_TYPE);
+        res->setIndexingMode(RT_TEXTURE_INDEX_NORMALIZED_COORDINATES);
+        res->setFilteringModes(RT_FILTER_LINEAR,RT_FILTER_LINEAR,RT_FILTER_NONE);
+        res->setMipLevelCount(1);
+        res->setArraySize(1);
+        res->setBuffer(0,0,tex);
+        res->validate();
+    }
+    else{
+        std::cout<<"Error reading texture: "<<name<<std::endl;
+    }
+    return res;
+}
+
+inline std::map<std::string,TextureSampler> loadTextures(const aiScene *s)
+{
+    ilInit();
+    std::map<std::string,TextureSampler> textureNameMap;
+    std::map<std::string,int> names;
+    int countTex=0;
+    for(unsigned int m=0; m< s->mNumMaterials; ++m)
+    {
+        int texIndex=0;
+        aiString path;
+        aiReturn texFound = s->mMaterials[m]->GetTexture(aiTextureType_DIFFUSE,texIndex,&path);
+        while(texFound==AI_SUCCESS)
+        {
+            texIndex++;
+            texFound = s->mMaterials[m]->GetTexture(aiTextureType_DIFFUSE,texIndex,&path);
+            countTex++;
+            names[path.data]=0;
+        }
+    }
+    ILuint imageIds[countTex];
+    ilGenImages(countTex,imageIds);
+    std::map<std::string,int>::iterator itr= names.begin();
+    for( int i=0; itr!=names.end(); itr++,i++)
+    {
+        std::string filename=itr->first;
+        textureNameMap[filename]=newTexture(filename);
+        std::cout<<"Successfully loaded texture: "<<filename<<std::endl;
+    }
+    return textureNameMap;
+}
+
+
+inline std::vector<Material> loadMaterials(const aiScene *s, std::map<std::string,TextureSampler> texMap, std::map<std::string,int> &matNameToIndex)
+{
+    std::vector<Material> res;
+    Program closest_hit_radiance = renderer->createProgramFromPTXFile(ptx_p,"closest_hit_radiance");
+    Program any_hit_shadow = renderer->createProgramFromPTXFile(ptx_p,"any_hit_shadow");
+
+    Buffer noBuffer = renderer->createBuffer(RT_BUFFER_INPUT,RT_FORMAT_BYTE4,1,1);
+
+    TextureSampler noTex=renderer->createTextureSampler();
+    noTex->setWrapMode(0,RT_WRAP_CLAMP_TO_EDGE);
+    noTex->setWrapMode(1,RT_WRAP_CLAMP_TO_EDGE);
+    noTex->setReadMode(RT_TEXTURE_READ_ELEMENT_TYPE);
+    noTex->setIndexingMode(RT_TEXTURE_INDEX_NORMALIZED_COORDINATES);
+    noTex->setFilteringModes(RT_FILTER_LINEAR,RT_FILTER_LINEAR,RT_FILTER_NONE);
+    noTex->setMipLevelCount(1);
+    noTex->setMaxAnisotropy(ANISOTROPY);
+    noTex->setArraySize(1);
+    noTex->setBuffer(0,0,noBuffer);
+
+    for(unsigned int m=0; m<s->mNumMaterials; m++)
+    {
+        Material optix_mat=renderer->createMaterial();
+        aiMaterial *mat = s->mMaterials[m];
+        aiString texPath;
+        aiString mat_name;
+        aiGetMaterialString(mat,AI_MATKEY_NAME,&mat_name);
+        std::cout<<"Loading material: "<<mat_name.data<<std::endl;
+        if(AI_SUCCESS==mat->GetTexture(aiTextureType_DIFFUSE,0,&texPath))
+        {
+            std::cout<<"Texture: "<<texPath.data<<std::endl;
+            optix_mat["tex0"]->setTextureSampler(texMap[texPath.data]);
+            optix_mat["texCount"]->setInt(1);
+        }
+        else
+        {
+            optix_mat["tex0"]->setTextureSampler(noTex);
+            optix_mat["texCount"]->setInt(0);
+        }
+        aiColor4D diffuse;
+        if(AI_SUCCESS==aiGetMaterialColor(mat,AI_MATKEY_COLOR_DIFFUSE,&diffuse))
+        {
+            float4 temp;
+            temp.x=diffuse.r;
+            temp.y=diffuse.g;
+            temp.z=diffuse.b;
+            temp.w=diffuse.a;
+            optix_mat["diffuse"]->setFloat(temp);
+            std::cout<<"Diffuse: "<<temp.x<<' '<<temp.y<<' '<<temp.z<<' '<<temp.w<<std::endl;
+        }
+        aiColor4D spec;
+        if(AI_SUCCESS==aiGetMaterialColor(mat,AI_MATKEY_COLOR_SPECULAR,&spec))
+        {
+            float4 temp;
+            temp.x=spec.r;
+            temp.y=spec.g;
+            temp.z=spec.b;
+            temp.w=spec.a;
+            optix_mat["specular"]->setFloat(temp);
+            std::cout<<"Specular: "<<temp.x<<' '<<temp.y<<' '<<temp.z<<' '<<temp.w<<std::endl;
+        }
+        float shininess;
+        if(AI_SUCCESS==aiGetMaterialFloat(mat,AI_MATKEY_SHININESS,&shininess))
+        {
+            optix_mat["shininess"]->setFloat(shininess);
+            std::cout<<"Shininess: "<<shininess<<std::endl;
+        }
+
+        std::cout<<"Loaded material: "<<mat_name.data<<std::endl;
+
+        optix_mat->setClosestHitProgram(Phong,closest_hit_radiance);
+        optix_mat->setAnyHitProgram(Shadow,any_hit_shadow);
+        res.push_back(optix_mat);
+        matNameToIndex[mat_name.data]=m;
+        //optix_mat->validate();
+    }
+    std::cout<<res.size()<<std::endl;
+    return res;
+}
+
+Acceleration newAccelerator(){
+    Acceleration acc=renderer->createAcceleration("Sbvh","Bvh");
+    return acc;
+}
+
+Acceleration newAcceleratorGeom(){
+    //Acceleration acc=renderer->createAcceleration("TriangleKdTree","KdTree");
+    Acceleration acc=renderer->createAcceleration("Sbvh","Bvh");
+    acc->setProperty("vertex_buffer_name","pos_buffer");
+    acc->setProperty("vertex_buffer_stride","4");
+    acc->setProperty("index_buffer_name","index_buffer");
+    acc->setProperty("index_buffer_stride","4");
+    return acc;
+}
+
+GeometryGroup loadGeometryGroup(aiNode* node, GeometryInstance meshes[])
+{
+    GeometryGroup geom_g=renderer->createGeometryGroup();
+    geom_g->setChildCount(node->mNumMeshes);
+    geom_g->setAcceleration(newAcceleratorGeom());
+    for(unsigned int m=0; m<node->mNumMeshes; m++)
+    {
+        GeometryInstance instance=meshes[node->mMeshes[m]];
+        geom_g->setChild(m,instance);
+    }
+    geom_g->validate();
+    return geom_g;
+}
+
+Transform loadNode(aiNode* node, GeometryInstance meshes[])
+{
+    Group child=renderer->createGroup();
+    aiMatrix4x4 trans = node->mTransformation;
+    Transform optix_trans=renderer->createTransform();
+    float mat_arr[16]={trans.a1, trans.b1, trans.c1, trans.d1,
+                       trans.a2, trans.b2, trans.c2, trans.d2,
+                       trans.a3, trans.b3, trans.c3, trans.d3,
+                       trans.a4, trans.b4, trans.c4, trans.d4};
+
+    /*std::cout<<mat_arr[0]<<' '<<mat_arr[1]<<' '<<mat_arr[2]<<' '<<mat_arr[3]<<std::endl
+             <<mat_arr[4]<<' '<<mat_arr[5]<<' '<<mat_arr[6]<<' '<<mat_arr[7]<<std::endl
+             <<mat_arr[8]<<' '<<mat_arr[9]<<' '<<mat_arr[10]<<' '<<mat_arr[11]<<std::endl
+             <<mat_arr[12]<<' '<<mat_arr[13]<<' '<<mat_arr[14]<<' '<<mat_arr[15]<<std::endl<<std::endl;*/
+
+    Matrix4x4 mat(mat_arr);
+    Matrix4x4 mat_inv=mat.inverse();
+    optix_trans->setMatrix(true,mat.getData(),mat_inv.getData());
+    optix_trans->setChild(child);
+    GeometryGroup geom_g=loadGeometryGroup(node,meshes);
+
+    child->setAcceleration(newAccelerator());
+    child->setChildCount(1+node->mNumChildren);
+    for(unsigned int m=0; m<node->mNumChildren;m++)
+    {
+        Transform t=loadNode(node->mChildren[m],meshes);
+        child->setChild(m,t);
+    }
+    child->setChild(node->mNumChildren,geom_g);
+    child->validate();
+    optix_trans->validate();
+    return optix_trans;
+}
+
+
+inline Group loadGeometry(const aiScene * s, std::vector<Material> materialVec)
+{
+    Buffer noTexCoord=renderer->createBuffer(RT_BUFFER_INPUT,RT_FORMAT_FLOAT2,1);
+    Program bounding_box = renderer->createProgramFromPTXFile(ptx_p,"boundingBoxMesh");
+    Program intersect = renderer->createProgramFromPTXFile(ptx_p,"intersectMesh");
+    GeometryInstance meshes[s->mNumMeshes];
+    for(unsigned int m=0; m<s->mNumMeshes; m++)
+    {
+        //std::cout<<"Loading mesh: "<<m<<std::endl;
+        //Initialize Geometry
+        //std::cout<<"Initializing"<<std::endl;
+        aiMesh * mesh=s->mMeshes[m];
+        Geometry optix_mesh=renderer->createGeometry();
+        optix_mesh->setPrimitiveCount(mesh->mNumFaces);
+        //copy indices buffer to optix
+        //std::cout<<"Loading Indices"<<std::endl;
+        optix_mesh->setPrimitiveCount(mesh->mNumFaces);
+        Buffer index_buffer = renderer->createBuffer(RT_BUFFER_INPUT,RT_FORMAT_INT3,mesh->mNumFaces);
+        int *temp_index=static_cast<int*>(index_buffer->map());
+        for(unsigned int i=0; i<mesh->mNumFaces; i++)
+        {
+            temp_index[3*i]=mesh->mFaces[i].mIndices[0];
+            temp_index[3*i+1]=mesh->mFaces[i].mIndices[1];
+            temp_index[3*i+2]=mesh->mFaces[i].mIndices[2];
+        }
+        index_buffer->unmap();
+        index_buffer->validate();
+        //copy vertices buffer
+        //std::cout<<"Loading Vertices"<<std::endl;
+        Buffer pos_buffer=renderer->createBuffer(RT_BUFFER_INPUT,RT_FORMAT_FLOAT3,mesh->mNumVertices);
+        void * temp_pos=pos_buffer->map();
+        memcpy(temp_pos,mesh->mVertices,mesh->mNumVertices*3*sizeof(float));
+        pos_buffer->unmap();
+        pos_buffer->validate();
+        //copy normals
+        //std::cout<<"Loading Normals"<<std::endl;
+        Buffer normal_buffer=renderer->createBuffer(RT_BUFFER_INPUT,RT_FORMAT_FLOAT3,mesh->mNumVertices);
+        void * temp_norm=normal_buffer->map();
+        memcpy(temp_norm,mesh->mNormals,mesh->mNumVertices*3*sizeof(float));
+        normal_buffer->unmap();
+        normal_buffer->validate();
+        //copy tex coordinates
+        Buffer texCoord_buffer;
+        if(mesh->HasTextureCoords(0))
+        {
+            //std::cout<<"Loading TexCoord"<<std::endl;
+            texCoord_buffer=renderer->createBuffer(RT_BUFFER_INPUT,RT_FORMAT_FLOAT2,mesh->mNumVertices);
+            float * temp_texCoord=static_cast<float*>(texCoord_buffer->map());
+            for(unsigned int i=0; i<mesh->mNumVertices; i++)
+            {
+                temp_texCoord[2*i]=mesh->mTextureCoords[0][i].x;
+                temp_texCoord[2*i+1]=mesh->mTextureCoords[0][i].y;
+                //std::cout<<"Loading TexCoord "<< mesh->mTextureCoords[0][i].x<<' '<<mesh->mTextureCoords[0][i].x <<std::endl;
+            }
+            texCoord_buffer->unmap();
+        }
+        //set atributes
+        //std::cout<<"Setting Attributes"<<std::endl;
+        optix_mesh["pos_buffer"]->set(pos_buffer);
+        optix_mesh["index_buffer"]->set(index_buffer);
+        optix_mesh["normal_buffer"]->set(normal_buffer);
+        if(mesh->HasTextureCoords(0)) {
+                optix_mesh["texCoord_buffer"]->set(texCoord_buffer);
+                optix_mesh["hasTexCoord"]->setInt(1);
+        }
+        else{
+            optix_mesh["texCoord_buffer"]->set(noTexCoord);
+            optix_mesh["hasTexCoord"]->setInt(0);
+        }
+        //set optix programs
+        //std::cout<<"Setting Programs"<<std::endl;
+        optix_mesh->setBoundingBoxProgram(bounding_box);
+        optix_mesh->setIntersectionProgram(intersect);
+        //create geometry instance
+        //std::cout<<"Instanctiating"<<std::endl;
+        GeometryInstance instance=renderer->createGeometryInstance();
+
+        instance->setGeometry(optix_mesh);
+        instance->setMaterialCount(1);
+        instance->setMaterial(0,materialVec[mesh->mMaterialIndex]);
+
+
+        //std::cout<<matName<<std::endl;
+        meshes[m]=instance;
+        //td::cout<<"Loaded mesh: "<<m<<std::endl<<std::endl;
+        optix_mesh->validate();
+        instance->validate();
+    }
+    Transform t=loadNode(s->mRootNode,meshes);
+    Group top = renderer->createGroup();
+    top->setChildCount(1);
+    top->setAcceleration(newAccelerator());
+    top->setChild(0,t);
+    top->validate();
+    return top;
+}
+
+
+
+void inline initContext()
+{
+    //create context
+    renderer=Context::create();
+    renderer->setRayTypeCount(RAY_TYPE_COUNT);
+    renderer["Phong"]->setInt(Phong);
+    renderer["Shadow"]->setInt(Shadow);
+
+    const aiScene * scene = loadScene(scene_p+scene_name);
+    std::map<std::string,TextureSampler> texMap=loadTextures(scene);
+    std::map<std::string,int> matNameToIndex;
+    std::vector<Material> materials=loadMaterials(scene,texMap,matNameToIndex);
+    Group top=loadGeometry(scene,materials);
+    renderer["top_object"]->set(top);
+
+    Program miss_radiance = renderer->createProgramFromPTXFile(ptx_p,"miss_radiance");
+    Program miss_shadow = renderer->createProgramFromPTXFile(ptx_p,"miss_shadow");
+
+    Program entryPoint=renderer->createProgramFromPTXFile(ptx_p,"pinhole_camera");
+    Program exept=renderer->createProgramFromPTXFile(ptx_p,"exception");
+
+    renderer->setEntryPointCount(1);
+    renderer->setRayGenerationProgram(0,entryPoint);
+    renderer->setExceptionProgram(0,exept);
+    renderer->setExceptionEnabled(RT_EXCEPTION_ALL,true);
+
+    renderer->setStackSize(8192);
+
+    renderer->setMissProgram(Phong,miss_radiance);
+    renderer->setMissProgram(Shadow,miss_shadow);
+
+    out=genOutputBuffer();
+    renderer["output0"]->set(out);
+
+    float3 eye=make_float3(3, 15, 0);
+    float3 up=make_float3(0.f,1.f,0.f);
+    float3 lookAt=make_float3(0.f, 15, 3.f);
+
+    float3 W=normalize(lookAt-eye);
+    float3 V=normalize(cross(up,-W));
+    float3 U=cross(-W,V);
+
+    renderer["eye"]->setFloat(eye);
+    renderer["U"]->setFloat(U);
+    renderer["V"]->setFloat(V);
+    renderer["W"]->setFloat(W);
+    renderer["fov"]->setFloat(1.f);
+
+    renderer->validate();
+}
+
+
+
+
+int main(int argc, char ** argv)
+{
+    //init glut
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE);
+    glutInitWindowPosition(0,0);
+    glutInitWindowSize(width,height);
+    glutCreateWindow("OptiX Ray Tracing Engine");
+    //callbacks
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(renderScene);
+    glutIdleFunc(renderScene);
+    //init glew
+    glewInit();
+    //setup optix
+    ilInit();
+    initContext();
+    //main loop
+    glutMainLoop();
+    return 0;
+}
---
+++ rt.cu
@@ -0,0 +1,188 @@
+#include <optix.h>
+#include <optixu/optixu_matrix.h>
+#include <optixu/optixu_math.h>
+#include <optixu/optixu_vector_types.h>
+#include <optixu/optixu_aabb.h>
+
+//camera properties
+rtDeclareVariable(float3,        eye, , );
+rtDeclareVariable(float3,        U, , );
+rtDeclareVariable(float3,        V, , );
+rtDeclareVariable(float3,        W, , );
+rtDeclareVariable(float,         fov, , );
+
+//ray types
+rtDeclareVariable(int, Phong, ,);
+rtDeclareVariable(int, Shadow, ,);
+
+//ray payloads
+struct PerRayDataRadiance{
+    float4 color;
+};
+
+struct PerRayDataShadow{
+    int hit;
+};
+
+rtDeclareVariable(PerRayDataRadiance, rad_res, rtPayload, );
+rtDeclareVariable(PerRayDataShadow, shadow_res, rtPayload, );
+
+//material variables
+rtTextureSampler<float4,2> tex0;
+rtDeclareVariable(float4, diffuse, ,);
+rtDeclareVariable(float4, specular, , );
+rtDeclareVariable(float, shininess, , );
+rtDeclareVariable(int, texCount, ,);
+
+//geomerty buffers
+rtBuffer<float3>pos_buffer;
+rtBuffer<float3>normal_buffer;
+rtBuffer<int3>index_buffer;
+rtBuffer<float2>texCoord_buffer;
+rtDeclareVariable(int, hasTexCoord, , );
+
+//intersection attributes
+rtDeclareVariable(float2, texCoord, attribute texCoord, );
+rtDeclareVariable(float3, geometric_normal, attribute geometric_normal, );
+rtDeclareVariable(float3, shading_normal, attribute shading_normal, );
+rtDeclareVariable(bool, useTex, attribute useTex, );
+rtDeclareVariable(float, t_hit, rtIntersectionDistance, );
+
+//ray and kernel size info
+rtDeclareVariable(optix::Ray, ray, rtCurrentRay, );
+rtDeclareVariable(uint2, launch_index, rtLaunchIndex, );
+rtDeclareVariable(uint2, launch_dim,   rtLaunchDim, );
+
+//output buffer
+rtDeclareVariable(rtObject, top_object, , );
+rtBuffer<float4,2> output0;
+
+RT_PROGRAM void pinhole_camera(){
+    float2 d = make_float2(launch_index) / make_float2(launch_dim) * 2.f - 1.f;
+	float3 ray_origin = eye;
+	float3 ray_direction = normalize(d.x*V*fov + d.y*U*fov + W);
+
+	optix::Ray ray = optix::make_Ray(ray_origin, ray_direction, Phong, 0.00000000001, RT_DEFAULT_MAX);
+    PerRayDataRadiance rad_res;
+    rad_res.color=make_float4(0.0f,0.0f,0.0f,0.0f);
+
+	rtTrace(top_object, ray, rad_res);
+
+	output0[launch_index] = rad_res.color;
+	//output0[launch_index] = make_float4(1.f,0.f,0.f,0.f);
+}
+
+RT_PROGRAM void exception(){
+    int code = rtGetExceptionCode();
+    if(code==RT_EXCEPTION_STACK_OVERFLOW){
+        output0[launch_index] = make_float4(1.f,0.f,0.f,0.f);
+    }
+}
+
+RT_PROGRAM void closest_hit_radiance(){
+    float4 color;
+
+    float3 world_geo_normal=normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, geometric_normal));
+	float3 world_shade_normal=normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, shading_normal));
+	float3 ffnormal=faceforward(world_shade_normal, -ray.direction, world_geo_normal);
+
+
+    float3 lightPos=make_float3(0.f,0.f,0.f);
+    float3 pos=ray.origin+ray.direction*t_hit;
+    float3 lightDir=normalize(lightPos-pos);
+
+    float intensity=max(dot(lightDir,ffnormal),0.0f);
+
+    if(texCount>0)
+    {
+        color=diffuse*intensity*tex2D(tex0,texCoord.x,texCoord.y);
+    }
+    else
+    {
+        color=diffuse*intensity;
+    }
+    PerRayDataShadow s;
+
+    float dist=length(lightPos-pos);
+    optix::Ray shadow = optix::make_Ray(pos,lightDir,Shadow,0.1, dist);
+    s.hit=0;
+    rtTrace(top_object,shadow,s);
+    if(s.hit>0){
+        color*=0.5;
+    }
+    rad_res.color=color;
+}
+
+RT_PROGRAM void any_hit_shadow(){
+    shadow_res.hit=1;
+    rtTerminateRay();
+}
+
+RT_PROGRAM void miss_radiance(){
+    rad_res.color=make_float4(0.f,1.f,0.f,0.f);
+    rtTerminateRay();
+}
+
+RT_PROGRAM void miss_shadow(){
+    shadow_res.hit=0;
+    rtTerminateRay();
+}
+
+RT_PROGRAM void intersectMesh(int primIdx){
+    //get indices
+    int3 id=index_buffer[primIdx];
+    //get vertices
+    float3 v1=pos_buffer[id.x];
+    float3 v2=pos_buffer[id.y];
+    float3 v3=pos_buffer[id.z];
+    //intersect ray with triangle
+    float3 n;
+    float t, beta, gamma;
+    if(intersect_triangle(ray, v1, v2, v3, n, t, beta, gamma))
+    {
+        if(rtPotentialIntersection(t))
+        {
+            //loading normals
+            float3 n1=normal_buffer[id.x];
+            float3 n2=normal_buffer[id.y];
+            float3 n3=normal_buffer[id.z];
+            //loading texCoords
+            if(hasTexCoord){
+                float2 t1=texCoord_buffer[id.x];
+                float2 t2=texCoord_buffer[id.y];
+                float2 t3=texCoord_buffer[id.z];
+                texCoord=(1.0f-beta-gamma)*t1 + beta*t2 +gamma*t3;
+                useTex=true;
+            }
+            else
+            {
+                texCoord=make_float2(1.0f,0.0f);
+                useTex=false;
+            }
+            //setting attributes
+            shading_normal=normalize((1.0f-beta-gamma)*n1 + beta*n2 +gamma*n3);
+            geometric_normal=normalize(n);
+            rtReportIntersection(0);
+        }
+    }
+}
+
+RT_PROGRAM void boundingBoxMesh(int primIdx, float result[6]){
+    //get indices
+    int3 id=index_buffer[primIdx];
+    //load vertices
+    float3 v1=pos_buffer[id.x];
+    float3 v2=pos_buffer[id.y];
+    float3 v3=pos_buffer[id.z];
+    const float area = length(cross(v2-v1,v3-v1));
+    optix::Aabb* aabb = (optix::Aabb*)result;
+    if(area>0.0f)
+    {
+        aabb->m_min=fminf(fminf(v1,v2),v3);
+        aabb->m_max=fmaxf(fmaxf(v1,v2),v3);
+    }
+    else
+    {
+        aabb->invalidate();
+    }
+}
